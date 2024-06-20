26478d5e8402274e0f6c7e536c22d188f00a3af2
AmySidon <108723547+AmySidon@users.noreply.github.com>
2024-06-20 02:28:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
 
-var randomNumber1 = Math.floor(Math.random() * 7);
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
 var randomImageSource = "images/dice" + randomNumber1+ ".png";
 
@@ -8,7 +8,7 @@ document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
 
 //----------
 
-var randomNumber2 = Math.floor(Math.random() * 7);
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var randomImageSource2 = "images/dice" + randomNumber2+ ".png";
 
